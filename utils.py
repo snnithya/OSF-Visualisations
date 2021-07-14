5e09062e27e31b9644dd6cc1e6815888fbd22792
snnithya <snnithya@gmail.com>
2021-07-14 13:29:27 -0400

    Made minor changes to the utils file including: 1. Changed default sample rate to 16k 2. Data or filename can be sent as parameters into the functions 3. Default parameters have been added to all functions. Exceptions are raised when required parameters aren't given 4. 'vibhaags' and 'matras' changed to 'provided' and 'computed' 5. Added a fig3.py file in Fig 3 folder with functions to take care of fig3 specific formats.

--- utils.py
+++ utils.py
@@ -9,45 +9,45 @@ from IPython.display import Audio
 import parselmouth
 sns.set_theme()
 
-def readCycleAnnotation(cyclePath, numDiv, startTime, endTime):
+def readCycleAnnotation(cyclePath, numDiv, startTime, duration):
     '''Function to read cycle annotation and add divisions in the middle if required.
 
     Parameters:
         cyclePath: path to the cycle annotation file
         numDiv: number of equally spaced divisions to add between pairs of annotations (numDiv - 1 timestamps will be added between each pair)
         startTime: start time of audio being analysed
-        endTime: end time of audio being analysed
+        duration: duration of the audio to be analysed
 
     Returns:
-        vibhaags: a numpy array of annotations from the file
-        matras: a numpy array of division between annotations
+        provided: a numpy array of annotations from the file
+        computed: a numpy array of division between annotations
     '''
 
     cycle_df = pd.read_csv(cyclePath)
-    index_values = cycle_df.loc[(cycle_df['Time'] >= startTime) & (cycle_df['Time'] <= endTime)].index.values
-    vibhaags = cycle_df.iloc[max(index_values[0]-1, 0):min(index_values[-1]+2, cycle_df.shape[0])]
+    index_values = cycle_df.loc[(cycle_df['Time'] >= startTime) & (cycle_df['Time'] <= startTime + duration)].index.values
+    provided = cycle_df.iloc[max(index_values[0]-1, 0):min(index_values[-1]+2, cycle_df.shape[0])]
     # add divisions in the middle
-    matras = []
-    for ind, vibhaag in enumerate(vibhaags['Time'].values[:-1]):
-        matras.extend(np.around(np.linspace(vibhaag, vibhaags['Time'].values[ind+1], num = numDiv, endpoint=False), 2)[1:])
+    computed = []
+    for ind, val in enumerate(provided['Time'].values[:-1]):
+        computed.extend(np.around(np.linspace(val, computed['Time'].values[ind+1], num = numDiv, endpoint=False), 2)[1:])
     
-    return vibhaags, matras
+    return provided, computed
 
-def drawAnnotation(cyclePath, numDiv, startTime, endTime, ax, c='purple'):
+def drawAnnotation(cyclePath, numDiv, startTime, duration, ax, c='purple'):
     '''Draws annotations on ax
 
     Parameters
         cyclePath: path to the cycle annotation file
         numDiv: number of equally spaced divisions to add between pairs of annotations (numDiv - 1 timestamps will be added between each pair)
         startTime: start time of audio being analysed
-        endTime: end time of audio being analysed
+        duration: duration of the audio to be analysed
         ax: axis to plot in
         c: colour to plot lines in
 
     Returns
         ax: axis that has been plotted in
     '''
-    vibhaags, matras = readCycleAnnotation(cyclePath, numDiv, startTime, endTime)
+    vibhaags, matras = readCycleAnnotation(cyclePath, numDiv, startTime, duration)
     if matras is not None:
         for matra in matras:
             ax.axvline(matra - startTime, linestyle='--', c=c)
@@ -57,15 +57,17 @@ def drawAnnotation(cyclePath, numDiv, startTime, endTime, ax, c='purple'):
             ax.annotate(vibhaag['Cycle'], (vibhaag['Time']-startTime, -0.4), bbox=dict(facecolor='grey', edgecolor='white'), c='white')
     return ax
 
-def pitchCountour(audioPath, startTime, endTime, minPitch, maxPitch, notes, tonic, timeStep=0.01, octaveJumpCost=0.9, veryAccurate=True, ax=None, freqXlabels=5, annotate=False, cyclePath=None, numDiv=0, xticks=False, yticks=False):
-    '''Returns pitch contour for the audio file
+def pitchCountour(audio=None, sr=16000, audioPath=None, startTime=0, duration=None, minPitch=98, maxPitch=660, notes=None, tonic=220, timeStep=0.01, octaveJumpCost=0.9, veryAccurate=True, ax=None, freqXlabels=5, annotate=False, cyclePath=None, numDiv=0, xticks=False, yticks=False):
+    '''Returns pitch contour for the audio
 
-    Uses `plotPitch` to plot pitch contour if ax is not None.
+    Uses `plotPitch` to plot pitch contour.
 
     Parameters
-        audioPath: path to audio file
+        audio: loaded audio time series
+        sr: sample rate of audio time series/ to load the audio at
+        audioPath: path to audio file; only needed if audio is None
         startTime: time to start reading audio file
-        endTime: time to end reading audio file
+        duration: duration of the audio file to read
         minPitch: minimum pitch to read for contour extraction
         maxPitch: maximum pitch to read for contour extraction
         notes: list of note objects indicating notes present in the raga
@@ -83,18 +85,22 @@ def pitchCountour(audioPath, startTime, endTime, minPitch, maxPitch, notes, toni
 
     Returns:
         ax: plot of pitch contour if ax was not None
-        pitch: pitch object extracted from audio if ax was None
+        
     '''
     
-    audio, sr = librosa.load(audioPath, sr=None, mono=True, offset=startTime, duration = endTime - startTime)
+    if audio is None:
+        # if audio is not given, load audio from audioPath
+        audio, sr = librosa.load(audioPath, sr=sr, mono=True, offset=startTime, duration = duration + startTime)
     snd = parselmouth.Sound(audio, sr)
     pitch = snd.to_pitch_ac(time_step=timeStep, pitch_floor=minPitch, very_accurate=veryAccurate, octave_jump_cost=octaveJumpCost, pitch_ceiling=maxPitch)
     
+    # if ax is None, raise error
+    if ax is None:
+        Exception('ax parameter has to be provided')
     # plot the contour
-    if ax is not None:  return plotPitch(pitch, notes, ax, tonic, startTime, endTime, freqXlabels, annotate=annotate, cyclePath=cyclePath, numDiv=numDiv, xticks=xticks, yticks=yticks)
-    else:   return pitch
+    return plotPitch(pitch, notes, ax, tonic, startTime, duration, freqXlabels, annotate=annotate, cyclePath=cyclePath, numDiv=numDiv, xticks=xticks, yticks=yticks)
 
-def plotPitch(pitch, notes, ax, tonic, startTime, endTime, freqXlabels=5, xticks=True, yticks=True, annotate=False, cyclePath=None, numDiv=0, cAnnot='purple'):
+def plotPitch(pitch=None, notes=None, ax=None, tonic=None, startTime=0, freqXlabels=5, xticks=True, yticks=True, annotate=False, cyclePath=None, numDiv=0, cAnnot='purple'):
     '''Converts the pitch contour from Hz to Cents, and plots it
 
     Parameters
@@ -102,42 +108,57 @@ def plotPitch(pitch, notes, ax, tonic, startTime, endTime, freqXlabels=5, xticks
         notes: object for each note used for labelling y-axis
         ax: axis object on which plot is to be plotted
         tonic: tonic (in Hz) of audio clip
+        startTime: start time for x labels in the plot
+        duration: duration of audio in the plot (used for x labels)
         freqXlabels: time (in seconds) after which each x label occurs
         annotate: if true will mark taala markings 
         xticks: if True, will print x tick labels
         yticks: if True, will print y tick labels
-        annotate: if True, 
+        annotate: if True, will add beat annotations to the plot 
     Returns
         ax: plotted axis
     '''
+    # Check that all required parameters are present
+    if pitch is None:
+        Exception('No pitch contour provided')
+    if tonic is None:
+        Exception('No tonic provided')
+    if ax is None:
+        Exception('ax parameter has to be provided')
     yvals = pitch.selected_array['frequency']
     yvals[yvals==0] = np.nan    # mark unvoiced regions as np.nan
     yvals[~(np.isnan(yvals))] = 1200*np.log2(yvals[~(np.isnan(yvals))]/tonic)   #convert Hz to cents
     xvals = pitch.xs()
+    duration = xvals[-1] + 1    # set duration as last x value + 1
     ax = sns.lineplot(x=xvals, y=yvals, ax=ax)
     ax.set(xlabel='Time Stamp (s)', 
     ylabel='Notes', 
     title='Pitch Contour (in Cents)', 
-    xlim=(0, endTime-startTime), 
-    xticks=np.arange(0, endTime-startTime, freqXlabels), 
-    xticklabels=np.around(np.arange(startTime, endTime, freqXlabels) )if xticks else [],
-    yticks=[x['cents'] for x in notes if (x['cents'] >= min(yvals)) & (x['cents'] <= max(yvals))] if yticks else [], 
-    yticklabels=[x['label'] for x in notes if (x['cents'] >= min(yvals)) & (x['cents'] <= max(yvals))] if yticks else [])
+    xlim=(0, duration), 
+    xticks=np.arange(0, duration, freqXlabels), 
+    xticklabels=np.around(np.arange(startTime, duration+startTime, freqXlabels) )if xticks else [])
+    if notes is not None and not yticks:
+        # add yticks if needed
+        ax.set(
+        yticks=[x['cents'] for x in notes if (x['cents'] >= min(yvals)) & (x['cents'] <= max(yvals))] if yticks else [], 
+        yticklabels=[x['label'] for x in notes if (x['cents'] >= min(yvals)) & (x['cents'] <= max(yvals))] if yticks else [])
 
     if annotate:
-        ax = drawAnnotation(cyclePath, numDiv, startTime, endTime, ax, c=cAnnot)
+        ax = drawAnnotation(cyclePath, numDiv, startTime, duration, ax, c=cAnnot)
     return ax
 
-def spectogram(audioPath, startTime, endTime, cmap, ax, amin=1e-5, freqXlabels=5, xticks=False, yticks=False, annotate=False, cyclePath=None, numDiv=0, cAnnot='purple'):
-    '''Plots spectogram
+def spectrogram(audio=None, sr=16000, audioPath=None, startTime=0, duration=None, cmap='Blues', ax=None, amin=1e-5, freqXlabels=5, xticks=False, yticks=False, annotate=False, cyclePath=None, numDiv=0, cAnnot='purple'):
+    '''Plots spectrogram
 
     Parameters
-        audioPath: path to the audio file
+        audio: loaded audio time series
+        sr: sample rate that audio time series is loaded/ is to be loaded in
+        audioPath: path to the audio file; only needed if audio is None
         startTime: time to start reading the audio at
-        endTime: time to stop reading audio at
-        cmap: colormap to use to plot spectogram
-        ax: axis to plot spectogram in
-        amin: controls the contrast of the spectogram; passed into librosa.power_to_db function
+        duration: duration of audio
+        cmap: colormap to use to plot spectrogram
+        ax: axis to plot spectrogram in
+        amin: controls the contrast of the spectrogram; passed into librosa.power_to_db function
         freqXlabels: time (in seconds) after which each x label occurs
         xticks: if true, will print x labels
         yticks: if true, will print y labels
@@ -146,7 +167,10 @@ def spectogram(audioPath, startTime, endTime, cmap, ax, amin=1e-5, freqXlabels=5
         numDiv: number of divisions to put between each annotation marking
         cAnnot: colour for the annotation marking
     '''
-    audio, sr = librosa.load(audioPath, sr=None, mono=True, offset=startTime, duration=endTime-startTime)
+    if ax is None:
+        Exception('ax parameter has to be provided')
+    if audio is None:
+        audio, sr = librosa.load(audioPath, sr=sr, mono=True, offset=startTime, duration=duration)
     
     # stft params
     winsize = int(np.ceil(sr*40e-3))
@@ -160,24 +184,26 @@ def spectogram(audioPath, startTime, endTime, cmap, ax, amin=1e-5, freqXlabels=5
     specshow(X_dB, x_axis='time', y_axis='linear', sr=sr, fmax=sr//2, hop_length=hopsize, ax=ax, cmap=cmap)
     ax.set(ylabel='Frequency (Hz)', 
     xlabel='', 
-    title='Spectogram',
-    xlim=(0, endTime-startTime), 
-    xticks=np.arange(0, endTime-startTime, freqXlabels) if xticks else [], 
-    xticklabels=np.around(np.arange(startTime, endTime, freqXlabels)) if xticks else [],
+    title='Spectrogram',
+    xlim=(0, duration), 
+    xticks=np.arange(0, duration, freqXlabels) if xticks else [], 
+    xticklabels=np.around(np.arange(startTime, duartion+startTime, freqXlabels)) if xticks else [],
     ylim=(0, 5000),
     yticks=[0, 2e3, 4e3] if yticks else [], 
     yticklabels=['0', '2k', '4k'] if yticks else [])
 
     if annotate:
-        ax = drawAnnotation(cyclePath, numDiv, startTime, endTime, ax, c=cAnnot)
+        ax = drawAnnotation(cyclePath, numDiv, startTime, duration, ax, c=cAnnot)
     return ax
 
-def drawWave(audioPath, startTime, endTime, ax, xticks=False, freqXlabels=5, annotate=False, cyclePath=None, numDiv=0, cAnnot='purple'):
+def drawWave(audio=None, sr=16000, audioPath=None, startTime=0, duration=None, ax=None, xticks=False, freqXlabels=5, annotate=False, cyclePath=None, numDiv=0, cAnnot='purple'):
     '''Plots the wave plot of the audio
 
+    audio: loaded audio time series
+    sr: sample rate that audio time series is loaded/ is to be loaded in
     audioPath: path to the audio file
     startTime: time to start reading the audio at
-    endTime: time to stop reading the audio at
+    duration: duration of audio to load
     ax: axis to plot waveplot in
     xticks: if True, will plot xticklabels
     freqXlabels: time (in seconds) after which each x label occurs
@@ -186,32 +212,36 @@ def drawWave(audioPath, startTime, endTime, ax, xticks=False, freqXlabels=5, ann
     numDiv: number of divisions to put between each annotation marking
     cAnnot: colour for the annotation marking
     '''
-    audio, sr = librosa.load(audioPath, sr=None, offset=startTime, duration=endTime-startTime)
+    if ax is None:
+        Exception('ax parameter has to be provided')
+    if audio is None:
+        audio, sr = librosa.load(audioPath, sr=sr, offset=startTime, duration=duration)
     waveplot(audio, sr, ax=ax)
     ax.set(xlabel='' if not xticks else 'Time (s)', 
-    xlim=(0, endTime-startTime), 
-    xticks=[] if not xticks else np.arange(0, endTime-startTime, freqXlabels),
-    xticklabels=[] if not xticks else np.around(np.arange(startTime, endTime, freqXlabels), 2),
+    xlim=(0, duration), 
+    xticks=[] if not xticks else np.arange(0, duration, freqXlabels),
+    xticklabels=[] if not xticks else np.around(np.arange(startTime, duration-startTime, freqXlabels), 2),
     title='Waveplot')
     if annotate:
-        ax = drawAnnotation(cyclePath, numDiv, startTime, endTime, ax, c=cAnnot)
+        ax = drawAnnotation(cyclePath, numDiv, startTime, duration, ax, c=cAnnot)
     return ax
 
-def playAudio(audioPath, startTime, endTime):
+def playAudio(audio=None, sr=16000, audioPath=None, startTime=0, duration=None):
     '''Plays relevant part of audio
 
     Parameters
         audioPath: file path to audio
         startTime: time to start reading audio at
-        endTime: time to stop reading audio at
+        duration: duration of the audio to load
 
     Returns:
         iPython.display.Audio object that plays the audio
     '''
-    audio, sr = librosa.load(audioPath, sr=None, offset=startTime, duration=endTime-startTime)
+    if audio is None:
+        audio, sr = librosa.load(audioPath, sr=None, offset=startTime, duration=duration)
     return Audio(audio, rate=sr)
 
-def generateFig(noRows, figSize, heightRatios):
+def generateFig(noRows, figSize=(14, 7), heightRatios=None):
     '''Generates a matplotlib.pyplot.figure and axes to plot in
 
     Axes in the plot are stacked vertically in one column, with height of each axis determined by heightRatios
@@ -227,6 +257,9 @@ def generateFig(noRows, figSize, heightRatios):
     '''
     if len(heightRatios) != noRows:
         Exception("Length of heightRatios has to be equal to noRows")
+    if heightRatios is None:
+        # if heightRatios is None
+        heightRatios = np.ones(noRows)
     fig = plt.figure(figsize=figSize)
     specs = fig.add_gridspec(noRows, 1, height_ratios = heightRatios)
     axs = [fig.add_subplot(specs[i, 0]) for i in range(noRows)]
